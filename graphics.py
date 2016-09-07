fdb4bb991f6b08cd115cbea07be91e014611c3a9
Max <maxs987@gmail.com>
2016-09-07 22:43:59 +0300

    main function for graphics

--- graphics.py
+++ graphics.py
@@ -1,1 +1,21 @@
-# here will be the graphics
+import os
+
+def print_field(matrix, obj=None):
+	if obj is None:
+		obj = []
+	for y,x in obj:
+		prev = matrix[x][y]
+		matrix[x][y] = 1
+
+	os.system('cls')
+	for row in matrix:
+		s = ''
+		for val in row:
+			if val:
+				s += '#'
+			else:
+				s += ' '
+		print(s)
+
+	for y,x in obj:
+		matrix[x][y] = prev
