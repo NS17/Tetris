4a0eb60f4c8f18dfc4d763ddc4f9d06f732fa56c
Max <maxs987@gmail.com>
2016-09-11 22:10:03 +0300

    fixed indent

--- graphics.py
+++ graphics.py
@@ -1,21 +1,21 @@
 import os
 
 def print_field(matrix, obj=None):
-	if obj is None:
-		obj = []
-	for y,x in obj:
-		prev = matrix[x][y]
-		matrix[x][y] = 1
+    if obj is None:
+        obj = []
+    for y,x in obj:
+        prev = matrix[x][y]
+        matrix[x][y] = 1
 
-	os.system('cls')
-	for row in matrix:
-		s = ''
-		for val in row:
-			if val:
-				s += '#'
-			else:
-				s += ' '
-		print(s)
+    os.system('cls')
+    for row in matrix[::-1]:
+        s = ''
+        for val in row:
+            if val:
+                s += '#'
+            else:
+                s += ' '
+        print(s)
 
-	for y,x in obj:
-		matrix[x][y] = prev
+    for y,x in obj:
+        matrix[x][y] = prev
