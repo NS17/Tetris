3b59fc3867554bb1f1375b5d9a7c06637b29bcff
Наташа <>
2016-09-12 19:48:21 +0300

    Functions

--- logic.py
+++ logic.py
@@ -1,17 +1,39 @@
 # here will be the logic of the program
 # here will be the logic of the program
-
- from graphics import print_field
+from graphics import print_field
  
 class Field:
 	def __init__(self, height, width):
+		self.width = width
+		self.height = height
 		self.part  = [[0] * width for i in range(height)]
-		self.object = [(0,0), (0,1), (0,2), (1,1)]
-		self.part[1][1] = self.part[1][2] = 1
+		self.object = [[0,0], [0,1], [0,2], [1,1]]
+		self.part[1][1] = self.part[1][2] = 0
+		for i in range(self.width):
+			self.part[3][i] = 1
+	def down(self):
+		for j in range(4):
+			if self.part[self.object[j][0]][self.object[j][1]+1] == 1:
+				return 0
+		for j in range(4):
+			self.object[j][1]+=1
+		return 1
+	def line(self):
+		for j in range(self.height):
+			l = 1
+			for i in range(self.width):
+				if self.part[j][i] == 0:
+					l=0
+			if l==1:
+				self.part.pop(j)
+				self.part.insert(0, [0]*self.width)
+			
 
-F = Field(5, 5)
-#F[]
+
+
+F = Field(10, 10)
+
 print_field(F.part, F.object)
-
-#print(F.object)
-print('Hi')
+#d = F.down()
+F.line()
+print_field(F.part, F.object)
