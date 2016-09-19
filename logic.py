dd467c76c9092c8deb12203db38b88d7fa9ef390
Наташа <>
2016-09-19 21:51:24 +0300

    Animation

--- logic.py
+++ logic.py
@@ -1,6 +1,8 @@
 # here will be the logic of the program
 # here will be the logic of the program
 from graphics import print_field
+
+import time
  
 class Field:
 	def __init__(self, height, width):
@@ -13,21 +15,21 @@ class Field:
 			#self.part[3][i] = 1
 	def down(self):
 		for x, y in self.object:
-			if self.part[x][y+1] == 1:
+			if y==self.height-1  or self.part[y+1][x]==1:
 				return False
 		for j in range(len(self.object)):
 			self.object[j][1]+=1
 		return True
 	def left(self):
 		for x, y in self.object:
-			if self.part[x+1][y] == 1 or x==0:
+			if self.part[y][x-1] == 1 or x==0:
 				return 
 		for j in range(len(self.object)):
 			self.object[j][0]-=1
 
 	def right(self):
 		for x, y in self.object:
-			if self.part[x+1][y] == 1 or x==self.width:
+			if self.part[y][x+1] == 1 or x==self.width-1:
 				return 
 		for j in range(len(self.object)):
 			self.object[j][0]+=1
@@ -44,25 +46,23 @@ class Field:
 		return lines
 
 	def place_object(self):
-		print(self.object)
 		for x, y in self.object:
-			self.part[x][y] = 1
+			self.part[y][x] = 1
 		self.object = [[0,0], [0,1], [0,2], [1,1]]
 
-			
-
-
-
 F = Field(10, 10)
+F.down()
+print_field(F)
+while F.down():
+	print_field(F)
+	time.sleep(0.8)
 
-#print_field(F.part, F.object)
-d = F.down()
-l = F.line()
-r = F.right()
-r = F.right()
-r = F.right()
-r = F.right()
-print(l)
-#print_field(F.part, F.object)
-F.place_object()
-#print_field(F.part, F.object)
+
+#d = F.down()
+#l = F.line()
+#r = F.right()
+#r = F.left()
+#print_field(F)
+#print_field(F)
+#F.place_object()
+#print_field(F)
